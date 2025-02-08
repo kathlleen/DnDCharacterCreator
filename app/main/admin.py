680a78a2646463406c4d97714b4ff86c3bfe1c84
kathlleen <katrina2005by@gmail.com>
2025-02-08 19:51:25 +0300

    Ability table

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from main.models import Language, Skill
+from main.models import Language, Skill,Ability
 # Register your models here.
 admin.site.register(Language)
 admin.site.register(Skill)
+admin.site.register(Ability)
 # admin.site.register(Proficiency)
--- app/main/models.py
+++ app/main/models.py
@@ -14,3 +14,13 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+class Ability(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    index = models.CharField(max_length=10, unique=True)
+    full_name = models.CharField(max_length=100)
+    description = models.TextField()
+    skills = models.ManyToManyField("Skill", blank=True)
+
+    def __str__(self):
+        return self.full_name
